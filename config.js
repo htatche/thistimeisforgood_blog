3fce0eea30e7ec7cd3d82faeb0b8bd1ef3efa9f9
Herve Tatche <herve.tatche@gmail.com>
2014-10-23 21:00:06 -0300

    Setting production URL for heroku

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'https://stormy-hamlet-1657.herokuapp.com/',
         mail: {},
         database: {
             client: 'sqlite3',
