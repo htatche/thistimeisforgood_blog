7cb6cd1bd3754f59de7a0df8e0b61ae66e5aa382
Herve Tatche <herve.tatche@gmail.com>
2014-11-02 00:07:11 -0300

    Effects on main page header

---
+++ content/themes/casper/assets/js/sticky-header.js
@@ -0,0 +1,60 @@
+// Effects for the main page
+
+(function( $ ){
+
+	$(document).ready(function () {
+
+		var last_pos = 0,
+				hidden_header = false,
+				animation = false // Prevents from calling the callback several times;
+
+	  function animateCallback(done) {
+			hidden_header = done || false;
+			animation = false; 
+		}
+
+		var hideHeader = function() {
+			animation = true;
+
+			$('html, body').animate(
+				{ scrollTop: $('#content').offset().top	},
+				"slow",
+				function() { animateCallback(true); } 
+			);			
+
+			$('.main-header-content').addClass('animated fadeOutRightBig');
+			$('.main-header-content').removeClass('fadeInRightBig');
+		}
+
+		var showHeader = function() {
+			animation = true;
+
+			$('html, body').animate(
+				{ scrollTop: 0 },
+				"slow",
+				function() { animateCallback(); }
+			);		
+
+			$('.main-header-content').addClass('animated fadeInRightBig');
+			$('.main-header-content').removeClass('fadeOutRightBig');
+		}		
+
+		$(window).scroll(function() {
+
+			var current_pos = $(window).scrollTop();
+
+			if (last_pos < current_pos) {
+				// We are scrolling down
+				if (!animation && !hidden_header && current_pos > 200) hideHeader();
+			} else {
+				// We are scrolling up
+				if (!animation && hidden_header && current_pos < 700)  showHeader();
+			}		
+
+			last_pos = $(window).scrollTop();
+
+		});
+
+	});
+
+})(jQuery);
