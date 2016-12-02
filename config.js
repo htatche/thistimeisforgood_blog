f69ca7d16d68eb1a20a5a7079e0c505f60e39741
Herve Tatche <herve.tatche@gmail.com>
2016-12-02 18:54:09 +1300

    chore(config) Tidy up config file

--- config.js
+++ config.js
@@ -1,46 +1,30 @@
-// # Ghost Configuration
-// Setup your Ghost install for various environments
-// Documentation can be found at http://support.ghost.org/config/
-
 var path = require('path'),
     config;
 
 config = {
-    // ### Production
-    // When running Ghost in the wild, use the production environment
-    // Configure your URL and mail settings here
     production: {
         url: 'http://blog.thistimeisforgood.com',
         mail: {},
         database: {
-            // client: 'sqlite3',
-            // connection: {
-            //     filename: path.join(__dirname, '/content/data/ghost.db')
-            // },
             client: 'postgres',
-				    connection: {
-		          host: process.env.POSTGRES_HOST,
-		          user: process.env.POSTGRES_USER,
-		          password: process.env.POSTGRES_PASSWORD,
-		          database: process.env.POSTGRES_DATABASE,
-		          port: '5432'
-				    },            
+            connection: {
+              host: process.env.POSTGRES_HOST,
+              user: process.env.POSTGRES_USER,
+              password: process.env.POSTGRES_PASSWORD,
+              database: process.env.POSTGRES_DATABASE,
+              port: '5432'
+            },
             debug: false,
             fileStorage: false
         },
-
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '0.0.0.0',
             port: process.env.PORT
 
         }
     },
 
-    // ### Development **(default)**
     development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blogs published URL.
         url: 'http://127.0.0.1:5000',
 
         // Example mail config
@@ -59,21 +43,21 @@ config = {
         // ```
 
         database: {
-            client: 'postgres',
+            // client: 'sqlite3',
             // connection: {
             //     filename: path.join(__dirname, '/content/data/ghost-dev.db')
             // },
-				    connection: {
-		          host: '127.0.0.1',
-		          user: 'htatche',
-		          password: 'postgrepass',
-		          database: 'blog',
-		          port: '5432'
-				    },
+            client: 'postgres',
+            connection: {
+              host: '127.0.0.1',
+              user: 'htatche',
+              password: 'postgrepass',
+              database: 'blog',
+              port: '5432'
+            },
             debug: false
         },
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             port: 5000
         },
@@ -82,11 +66,6 @@ config = {
         }
     },
 
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
     testing: {
         url: 'http://127.0.0.1:2369',
         database: {
@@ -102,26 +81,6 @@ config = {
         logging: false
     },
 
-    // ### Testing MySQL
-    // Used by Travis - Automated testing run through GitHub
-    'testing-mysql': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'mysql',
-            connection: {
-                host     : '127.0.0.1',
-                user     : 'root',
-                password : '',
-                database : 'ghost_testing',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
 
     // ### Testing pg
     // Used by Travis - Automated testing run through GitHub
@@ -145,5 +104,4 @@ config = {
     }
 };
 
-// Export config
 module.exports = config;
